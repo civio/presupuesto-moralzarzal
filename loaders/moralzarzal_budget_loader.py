ce24d1b19eb52f0203b3287fa862ce5eca9c1207
Eduardo Sebastian <esebastian@altiora.es>
2018-05-31 18:58:32 +0200

    Update 2017 budget data
    civio/presupuesto-management#737

--- loaders/moralzarzal_budget_loader.py
+++ loaders/moralzarzal_budget_loader.py
@@ -1,85 +1,109 @@
 # -*- coding: UTF-8 -*-
 from budget_app.loaders import SimpleBudgetLoader
-from budget_app.models import *
-from decimal import *
-import csv
-import os
-import re
+
+
+expenses_mapping = {
+    'default': {'ic_code': None, 'fc_code': 1, 'full_ec_code': 2, 'description': 3, 'forecast_amount': 7, 'actual_amount': 10},
+    '2017': {'ic_code': None, 'fc_code': 1, 'full_ec_code': 2, 'description': 4, 'forecast_amount': 8, 'actual_amount': 11},
+}
+
+income_mapping = {
+    'default': {'full_ec_code': 2, 'description': 3, 'forecast_amount': 4, 'actual_amount': 7},
+}
+
+programme_mapping = {
+    # old programme: new programme
+    '1340': '1350',     # Protección Civil
+    '1350': '1360',     # Servicio de prevención y extinción de incendios
+    '1530': '1521',     # Promoción y gestión de vivienda de protección pública
+    '1550': '1530',     # Vías públicas
+    '3230': '3270',     # Fomento de la convivencia ciudadana
+    '3240': '3260',     # Servicios complementarios de educación
+}
+
+
+class MoralzarzalBudgetCsvMapper:
+    def __init__(self, year, is_expense):
+        column_mapping = income_mapping
+
+        if is_expense:
+            column_mapping = expenses_mapping
+
+        mapping = column_mapping.get(str(year))
+
+        if not mapping:
+            mapping = column_mapping.get('default')
+
+        self.ic_code = mapping.get('ic_code')
+        self.fc_code = mapping.get('fc_code')
+        self.full_ec_code = mapping.get('full_ec_code')
+        self.description = mapping.get('description')
+        self.forecast_amount = mapping.get('forecast_amount')
+        self.actual_amount = mapping.get('actual_amount')
+
 
 class MoralzarzalBudgetLoader(SimpleBudgetLoader):
+    # make year data available in the class and call super
+    def load(self, entity, year, path, status):
+        self.year = year
+        SimpleBudgetLoader.load(self, entity, year, path, status)
 
     # Parse an input line into fields
     def parse_item(self, filename, line):
-        # Programme codes have changed in 2015, due to new laws. Since the application expects a code-programme
-        # mapping to be constant over time, we are forced to amend budget data prior to 2015.
-        # See https://github.com/dcabo/presupuestos-aragon/wiki/La-clasificaci%C3%B3n-funcional-en-las-Entidades-Locales
-        programme_mapping = {
-        # old programme: new programme
-            '1340': '1350',     # Protección Civil
-            '1350': '1360',     # Servicio de prevención y extinción de incendios
-            '1530': '1521',     # Promoción y gestión de vivienda de protección pública
-            '1550': '1530',     # Vías públicas
-            '3230': '3270',     # Fomento de la convivencia ciudadana
-            '3240': '3260',     # Servicios complementarios de educación
-        }
-    
-        # On economic codes we get the first three digits (everything but last two)
-        ec_code = line[2].strip()
-        ec_code = ec_code[:-2]
+        # Type of data
+        is_expense = (filename.find('gastos.csv') != -1)
+        is_actual = (filename.find('/ejecucion_') != -1)
 
-        # Item numbers are the last two digits from the economic codes (fourth and fifth digit)
-        item_number = line[2].strip()
-        item_number = item_number[-2:]
+        # Mapper
+        mapper = MoralzarzalBudgetCsvMapper(self.year, is_expense)
 
-        # Institutional code (all income goes to the root node)
+        # Institutional code
+        # All expenses go to the root node
         ic_code = '000'
 
-        # Description
-        description = line[3].strip()
+        # Economic code
+        full_ec_code = line[mapper.full_ec_code].strip()
 
-        # Type of data
-        is_expense = (filename.find('gastos.csv')!=-1)
-        is_actual = (filename.find('/ejecucion_')!=-1)
+        # Concepts are the firts three digits from the economic codes
+        ec_code = full_ec_code[:3]
+
+        # Item numbers are the last two digits from the economic codes (fourth and fifth digits)
+        item_number = full_ec_code[-2:]
+
+        # Description
+        description = line[mapper.description].strip()
+
+        # Parse amount
+        amount = line[mapper.actual_amount if is_actual else mapper.forecast_amount]
+        amount = self._parse_amount(amount)
 
         # Expenses
         if is_expense:
+            # Functional code
             # We got 3- or 4- digit functional codes as input, so add a trailing zero
-            fc_code = line[1].strip()
+            fc_code = line[mapper.fc_code].strip()
             fc_code = fc_code.ljust(4, '0')
 
+            # Programme codes have changed in 2015, due to new laws. Since the application expects a code-programme
+            # mapping to be constant over time, we are forced to amend budget data prior to 2015.
+            # See https://github.com/dcabo/presupuestos-aragon/wiki/La-clasificaci%C3%B3n-funcional-en-las-Entidades-Locales
             # For years before 2015 we check whether we need to amend the programme code
-            year = re.search('municipio/(\d+)/', filename).group(1)
-            if int(year) < 2015:
+            if int(self.year) < 2015:
                 fc_code = programme_mapping.get(fc_code, fc_code)
-
-            # Parse amount
-            amount = line[10 if is_actual else 7].strip()
-            amount = self._parse_amount(amount)
-
-
-            return {
-                'is_expense': True,
-                'is_actual': is_actual,
-                'fc_code': fc_code,
-                'ec_code': ec_code,
-                'ic_code': ic_code,
-                'item_number': item_number,
-                'description': description,
-                'amount': amount
-            }
 
         # Income
         else:
-            # Parse amount
-            amount = line[7 if is_actual else 4].strip()
-            amount = self._parse_amount(amount)
+            # Functional code
+            # We don't have a functional code in income
+            fc_code = None
 
-            return {
-                'is_expense': False,
-                'is_actual': is_actual,
-                'ec_code': ec_code,
-                'ic_code': ic_code,
-                'item_number': item_number,
-                'description': description,
-                'amount': amount
-            }
+        return {
+            'is_expense': is_expense,
+            'is_actual': is_actual,
+            'fc_code': fc_code,
+            'ec_code': ec_code,
+            'ic_code': ic_code,
+            'item_number': item_number,
+            'description': description,
+            'amount': amount
+        }
--- loaders/moralzarzal_payments_loader.py
+++ loaders/moralzarzal_payments_loader.py
@@ -1,26 +1,50 @@
 # -*- coding: UTF-8 -*-
 from budget_app.loaders import PaymentsLoader
 from budget_app.models import Budget
+
 import dateutil.parser
 import re
 
-class MoralzarzalPaymentsLoader(PaymentsLoader):
 
+payments_mapping = {
+    'default': {'fc_code': 2, 'date': 5, 'payee': 7, 'description': 8, 'amount': 9},
+    '2017': {'fc_code': 1, 'date': 3, 'payee': 5, 'description': 6, 'amount': 7},
+}
+
+
+class MoralzarzalPaymentsCsvMapper:
+    def __init__(self, year):
+        mapping = payments_mapping.get(str(year))
+
+        if not mapping:
+            mapping = payments_mapping.get('default')
+
+        self.fc_code = mapping.get('fc_code')
+        self.date = mapping.get('date')
+        self.payee = mapping.get('payee')
+        self.description = mapping.get('description')
+        self.amount = mapping.get('amount')
+
+
+class MoralzarzalPaymentsLoader(PaymentsLoader):
     # Parse an input line into fields
     def parse_item(self, budget, line):
+        # Mapper
+        mapper = MoralzarzalPaymentsCsvMapper(budget.year)
+
         # First two digits of the programme make the policy id
         # But what we want as area is the policy description
-        policy_id = line[2].strip()[:2]
+        policy_id = line[mapper.fc_code].strip()[:2]
         policy = Budget.objects.get_all_descriptions(budget.entity)['functional'][policy_id]
 
         # We use dateutil.parser to parse the date no matter which format is used and then set
         # the format to the one expected by the item loader
-        date = line[5].strip()
+        date = line[mapper.date].strip()
         date = dateutil.parser.parse(date, dayfirst=True).strftime("%Y-%m-%d")
 
         # Normalize payee data
         # remove triling spaces
-        payee = line[7].strip()
+        payee = line[mapper.payee].strip()
         # remove commas
         payee = payee.replace(', ', ' ').replace(',', ' ')
         # normalize company types
@@ -49,17 +73,18 @@ class MoralzarzalPaymentsLoader(PaymentsLoader):
 
         # We truncate the description to the maximum length supported in the data model
         # and fix enconding errors
-        description = line[8].strip()[:300]
+        description = line[mapper.description].strip()[:300]
         description = description.decode('utf-8','ignore').encode('utf-8')
 
         # Parse amount
-        amount = line[9].strip()
+        amount = line[mapper.amount].strip()
         amount = self._read_english_number(amount)
 
         return {
             'area': policy,
             'programme': None,
-            'fc_code': None,  # We don't try (yet) to have foreign keys to existing records
+            'ic_code': None,
+            'fc_code': None,
             'ec_code': None,
             'date': date,
             'payee': payee,
